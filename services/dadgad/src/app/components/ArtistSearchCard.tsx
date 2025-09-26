1b5ea64a93b766c18026816fcf60891437d3b397
Jesse Shaw <JesseShawCodes@gmail.com>
2025-09-26 16:33:28 -0400

    Package.json scripts, added in typescript, tsconfig, revised layout, navbar, ArtistSearchCard

--- services/dadgad/src/app/components/ArtistSearchCard.tsx
+++ services/dadgad/src/app/components/ArtistSearchCard.tsx
@@ -4,11 +4,41 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import myImage from '../logo.png';
 import { faMusic } from '@fortawesome/free-solid-svg-icons';
 
-export default function ArtistSearchCard({artistResult}) {
+type ArtistAttributes = {
+  genreNames: string[];
+  name: string;
+  url: string;
+  artwork?: {
+      url: string;
+      width?: number;
+      height?: number;
+  }
+};
+
+type ArtistRelationships = {
+  albums: {
+    href: string;
+    data: any[]; // Replace any with a more specific type if you have it
+  };
+};
+
+type Artist = {
+  id: string;
+  type: string;
+  href: string;
+  attributes: ArtistAttributes;
+  relationships: ArtistRelationships;
+};
+
+type ArtistSearchCardProps = {
+  artistResult: Artist;
+};
+
+export default function ArtistSearchCard({artistResult}: ArtistSearchCardProps) {
   return (
     <div className="mt-4 mx-4 card artist-search-card g-col-6 g-col-md-4" key={artistResult.id}>
       {
-        Object.prototype.hasOwnProperty.call(artistResult.attributes, 'artwork') ? <img src={artistResult.attributes.artwork.url} className="card-img-top" alt={`${artistResult.attributes.name} promo`} /> : 
+        artistResult.attributes.artwork ? <Image src={artistResult.attributes.artwork.url} className="card-img-top" alt={`${artistResult.attributes.name} promo`} width={100} height={100} /> : 
         <div className="h-100 placeholder-image-container d-flex justify-content-center align-items-center">
           <div className="d-flex flex-column">
             <Image src={myImage} alt="Dadgad logo" role="presentation" className="me-2" style={{width: '50px', height: 'auto'}}/>
--- services/dadgad/src/app/layout.tsx
+++ services/dadgad/src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata = {
   }
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   const mainLayout = (
     <ReduxProvider>
       <BracketContext>
